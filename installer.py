acfdfa8faa91070d37987cd7c46357aa98380491
Ben Meadors <benmmeadors@gmail.com>
2022-01-28 14:00:09 -0600

    Set window title

--- installer.py
+++ installer.py
@@ -9,6 +9,8 @@ class Form(QDialog):
 
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
+        
+        self.setWindowTitle("Meshtastic Installer")
 
         # Create widgets
         self.select_firmware = QPushButton("Select firmware")
@@ -56,7 +58,6 @@ if __name__ == '__main__':
 
     # Create the Qt Application
     app = QApplication(sys.argv)
-
     # Create and show the form
     form = Form()
     form.show()
