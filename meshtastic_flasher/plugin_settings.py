ed12127695e6e0c8a9419309b1d9afbdef7e6fc4
Mike Kinney <mike.kinney@gmail.com>
2022-02-18 14:06:26 -0800

    add plugins: range test, env measure, and external notifications

---
+++ meshtastic_flasher/plugin_settings.py
@@ -0,0 +1,66 @@
+"""class for the plugin settings"""
+
+
+from PySide6.QtWidgets import QTabWidget, QMainWindow
+
+from meshtastic_flasher.plugins_range_test_form import RangeTestForm
+from meshtastic_flasher.plugins_external_notifications_form import ExternalNotificationsForm
+from meshtastic_flasher.plugins_environmental_measurement_form import EnvironmentalMeasurementForm
+
+
+class PluginSettings(QMainWindow):
+    """plugin settings"""
+
+    def __init__(self):
+        """constructor"""
+        super(PluginSettings, self).__init__()
+
+        self.port = None
+        self.interface = None
+
+        width = 800
+        height = 600
+        self.setMinimumSize(width, height)
+        self.setWindowTitle("Plugin Settings")
+
+        self.range_test_form = RangeTestForm(self)
+        self.external_notifications_form= ExternalNotificationsForm(self)
+        self.envrionmental_measurement_form= EnvironmentalMeasurementForm(self)
+
+        self.tabs = QTabWidget()
+
+        self.tabs.blockSignals(True) # just for not showing initial message
+        self.tabs.currentChanged.connect(self.on_change_tabs)
+
+        self.tabs.setTabPosition(QTabWidget.North)
+
+        self.tabs.addTab(self.range_test_form, "Range Test")
+        self.tabs.addTab(self.external_notifications_form, "Notifications")
+        self.tabs.addTab(self.envrionmental_measurement_form, "Environment")
+
+        self.setCentralWidget(self.tabs)
+
+        self.tabs.blockSignals(False) # now listen the currentChanged signal
+
+
+    def on_change_tabs(self, i):
+        """On change of each tab """
+        print(f'on_change_tabs:{i}')
+        if i == 0:
+            print('range_test_form.run()')
+            self.range_test_form.run(port=self.port, interface=self.interface)
+        elif i == 1:
+            print('external notifications run()')
+            self.external_notifications_form.run(port=self.port, interface=self.interface)
+        elif i == 2:
+            print('envrionmental_measurement_form run()')
+            self.envrionmental_measurement_form.run(port=self.port, interface=self.interface)
+
+
+    def run(self, port=None, interface=None):
+        """load the form"""
+        print(f'in plugin settings run() port:{port}:')
+        self.port = port
+        self.interface = interface
+        self.show()
+        self.range_test_form.run(port=self.port, interface=self.interface)
---
+++ meshtastic_flasher/plugins_external_notifications_form.py
@@ -0,0 +1,132 @@
+"""class for the plugin external notifications settings"""
+
+
+from PySide6.QtWidgets import QDialog, QCheckBox, QFormLayout, QDialogButtonBox, QLineEdit
+
+import meshtastic.serial_interface
+import meshtastic.util
+import meshtastic.mesh_pb2
+import meshtastic.radioconfig_pb2
+from meshtastic.__init__ import BROADCAST_ADDR
+from meshtastic.__main__ import setPref
+from meshtastic_flasher.util import zero_if_blank
+
+
+class ExternalNotificationsForm(QDialog):
+    """plugin external notifications form"""
+
+    def __init__(self, parent=None):
+        """constructor"""
+        super(ExternalNotificationsForm, self).__init__(parent)
+
+        self.parent = parent
+
+        width = 500
+        height = 200
+        self.setMinimumSize(width, height)
+        self.setWindowTitle("Plugins External Notifications Settings")
+
+        self.port = None
+        self.interface = None
+        self.prefs = None
+
+        # Create widgets
+        self.ext_notification_plugin_active = QCheckBox()
+        self.ext_notification_plugin_alert_bell = QCheckBox()
+        self.ext_notification_plugin_alert_message = QCheckBox()
+        self.ext_notification_plugin_enabled = QCheckBox()
+        self.ext_notification_plugin_output = QLineEdit()
+        self.ext_notification_plugin_output_ms = QLineEdit()
+
+        # Add a button box
+        self.button_box = QDialogButtonBox()
+        self.button_box.setStandardButtons(QDialogButtonBox.Ok)
+        self.button_box.accepted.connect(self.accept)
+        self.button_box.rejected.connect(self.reject)
+
+        # create form
+        form_layout = QFormLayout()
+        form_layout.addRow(self.tr("Active?"), self.ext_notification_plugin_active)
+        form_layout.addRow(self.tr("Alert bell?"), self.ext_notification_plugin_alert_bell)
+        form_layout.addRow(self.tr("Alert message?"), self.ext_notification_plugin_alert_message)
+        form_layout.addRow(self.tr("Enabled?"), self.ext_notification_plugin_enabled)
+        form_layout.addRow(self.tr("Output"), self.ext_notification_plugin_output)
+        form_layout.addRow(self.tr("Output (ms)"), self.ext_notification_plugin_output_ms)
+        form_layout.addRow(self.tr(""), self.button_box)
+        self.setLayout(form_layout)
+
+
+    def run(self, port=None, interface=None):
+        """load the form"""
+        self.port = port
+        self.interface = interface
+        if self.port:
+            print(f'using port:{self.port}')
+            self.get_values()
+            self.show()
+
+
+    def get_values(self):
+        """Get values from device"""
+        try:
+            if self.interface is None:
+                print('interface was none?')
+                self.interface = meshtastic.serial_interface.SerialInterface(devPath=self.port)
+            if self.interface:
+                self.prefs = self.interface.getNode(BROADCAST_ADDR).radioConfig.preferences
+
+                if self.prefs.ext_notification_plugin_active:
+                    self.ext_notification_plugin_active.setChecked(True)
+
+                if self.prefs.ext_notification_plugin_alert_bell:
+                    self.ext_notification_plugin_alert_bell.setChecked(True)
+
+                if self.prefs.ext_notification_plugin_alert_message:
+                    self.ext_notification_plugin_alert_message.setChecked(True)
+
+                if self.prefs.ext_notification_plugin_enabled:
+                    self.ext_notification_plugin_enabled.setChecked(True)
+
+                if self.prefs.ext_notification_plugin_output:
+                    self.ext_notification_plugin_output.setText(f'{self.prefs.ext_notification_plugin_output}')
+                else:
+                    self.ext_notification_plugin_output.setText("0")
+
+                if self.prefs.ext_notification_plugin_output_ms:
+                    self.ext_notification_plugin_output_ms.setText(f'{self.prefs.ext_notification_plugin_output_ms}')
+                else:
+                    self.ext_notification_plugin_output_ms.setText("0")
+
+        except Exception as e:
+            print(f'Exception:{e}')
+
+
+    def write_values(self):
+        """Write values to device"""
+        try:
+            if self.interface:
+                # TODO: Should we only write if we changed values?
+                print("Writing preferences to device")
+                prefs = self.interface.getNode(BROADCAST_ADDR).radioConfig.preferences
+                setPref(prefs, 'ext_notification_plugin_active', f'{self.ext_notification_plugin_active.isChecked()}')
+                setPref(prefs, 'ext_notification_plugin_alert_bell', f'{self.ext_notification_plugin_alert_bell.isChecked()}')
+                setPref(prefs, 'ext_notification_plugin_alert_message', f'{self.ext_notification_plugin_alert_message.isChecked()}')
+                setPref(prefs, 'ext_notification_plugin_enabled', f'{self.ext_notification_plugin_enabled.isChecked()}')
+                setPref(prefs, 'ext_notification_plugin_output', zero_if_blank(self.ext_notification_plugin_output.text()))
+                setPref(prefs, 'ext_notification_plugin_output_ms', zero_if_blank(self.ext_notification_plugin_output_ms.text()))
+                self.interface.getNode(BROADCAST_ADDR).writeConfig()
+
+        except Exception as e:
+            print(f'Exception:{e}')
+
+
+    def reject(self):
+        """Cancel without saving"""
+        print('CANCEL button was clicked')
+        self.parent.my_close()
+
+
+    def accept(self):
+        """Close the form"""
+        print('SAVE button was clicked')
+        self.write_values()
