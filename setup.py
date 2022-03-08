aef27a61964837a0c0539d2a801a0828db8362ea
Mike Kinney <mike.kinney@gmail.com>
2022-03-07 22:51:38 -0800

    need updated meshtastic due to a bugfix

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     package_data={
         '': ['logo.png', 'help.svg', 'info.svg', 'options.svg', 'cog.svg', 'meshtastic_theme.xml', 'fields.json'],
     },
-    install_requires=["pyside6", "PyGithub", "esptool", "meshtastic>=1.2.85", "qt-material",
+    install_requires=["pyside6", "PyGithub", "esptool", "meshtastic>=1.2.89", "qt-material",
                       "psutil", "adafruit-nrfutil", "pyserial", "geocoder"],
     extras_require={
     },
