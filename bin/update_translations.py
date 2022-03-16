7c4224c67a5bf9ea204f98a8d94c129e524562e6
Mike Kinney <mike.kinney@gmail.com>
2022-03-16 10:03:14 -0700

    process the zh lang review by laosb

---
+++ bin/update_translations.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""Update the fields.json file from an updated file that was created using show_entries_for_lang.py and edited"""
+
+import sys
+
+from meshtastic_flasher.util import load_fields, write_fields
+
+# simple arg check
+if len(sys.argv) < 2:
+    print(f"usage: {sys.argv[0]} input_file")
+    print("  example: foo.txt")
+    sys.exit(3)
+
+filename = sys.argv[1].lower()
+print(f'filename:{filename}')
+
+
+field = None
+key = None
+en_entry = None
+tr_entry = None
+
+fields = load_fields()
+
+with open(filename, encoding='utf-8') as input_file:
+    lines = input_file.readlines()
+
+    lang = None
+    if lines[0].startswith('lang:'):
+        tmp = lines[0]
+        lang = tmp[5:].strip()
+    print(f'lang:{lang}:')
+
+    for line in lines:
+        if line.startswith('** field:'):
+            tmp = line.split(' ')
+            tmp2 = tmp[1].split(':')
+            tmp3 = tmp[2].split(':')
+            field = tmp2[1]
+            key = tmp3[1]
+            print(f'field:{field} key:{key}')
+            en_entry = None
+            tr_entry = None
+
+        if line.startswith('en:'):
+            en_entry = line[3:].strip()
+            print(f'en_entry:{en_entry}')
+
+        if line.startswith(f'{lang}:'):
+            tr_entry = line[3:]
+            print(f'tr_entry:{tr_entry}')
+            fields[field][key][lang] = tr_entry.strip()
+
+write_fields(fields)
